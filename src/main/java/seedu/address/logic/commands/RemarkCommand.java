8ee1a1f29d76a4bb0fd2a3584bf9e4926dbf0b15
AmyLiiiii <li_zhaohan@u.nus.edu>
2026-09-15 16:07:30 +0800

    Parse remark command arguments

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -18,22 +19,15 @@ public class RemarkCommand extends Command {
             + "by the index number used in the last person listing. "
             + "Existing remark will be overwritten by the input.\n"
             + "Parameters: INDEX (must be a positive integer) "
-            + "r/ [REMARK]\n"
+            + PREFIX_REMARK + " [REMARK]\n"
             + "Example: " + COMMAND_WORD + " 1 "
-            + "r/ Likes to swim.";
+            + PREFIX_REMARK + " Likes to swim.";
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
 
     private final Index index;
     private final String remark;
-
-    /**
-     * Creates a temporary {@code RemarkCommand} while user input parsing is not implemented yet.
-     */
-    public RemarkCommand() {
-        this(Index.fromOneBased(1), "");
-    }
 
     /**
      * @param index of the person in the filtered person list to edit the remark
